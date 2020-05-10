f11e46d7b6aa2b771e6eeb385aab896bbab283e0
Sebin Sunny <me@sebinsunny.com>
2020-05-10 20:59:21 +1000

    removed the binding addingp progress bar

--- partone/app.js
+++ partone/app.js
@@ -6,11 +6,11 @@ new Vue({
 
             eventDate: 'August 14th - 16th',
             eventTitle: 'Summer Festival!',
+            signUptext: 'Add your name to the guest list for <em>exclusive</em> offers:',
             eventDescription: 'It\'s back! This years summer festival will be in the beautiful countryside featuring our best line up ever!'
         },
         newNameText: '',
         guestName: [],
-        formSubmitClass: ""
 
     },
     methods: {
@@ -18,7 +18,7 @@ new Vue({
             if (this.newNameText.length > 0) {
                 this.guestName.push(this.newNameText);
                 this.newNameText = ""
-                this.formSubmitClass = "submitted"
+
             }
         }
     }
